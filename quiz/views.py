37a0f2403e207d9532aa46be804472a6f117d51e
Vertsi <v.vikstedt@gmail.com>
2017-09-27 13:14:44 +0200

    Up until task 16

--- quiz/views.py
+++ quiz/views.py
@@ -1,16 +1,51 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render
+quizzes = [
+	{
+		"quiz_number": 1, 
+		"name": "Klassiska böcker",	
+		"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2, 
+		"name": "Största fotbollslage",	
+		"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3, 
+		"name": "Världens mest kända hackare",	
+		"description": "Kan du din hackershistoria?"
+	},
+]
 
 def startpage (request):
-	return render(request, "start.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "start.html", context)
 
 def quiz(request, quiz_number):
-	return render(request, "quiz.html")
+	context = {
+		"quiz": quizzes[quiz_number - 1],
+		"quiz_number": quiz_number, 
+	}
+	return render(request, "quiz.html", context)
 
 def question(request, quiz_number, question_number):
-	return render(request, "question.html")
+	context = {
+		"question_number": question_number,
+		"question": "Hur många bultar har ölandsbron?",
+		"answer1": "12",
+		"answer2": "66 400",
+		"answer3": "7 428 954",
+		"quiz_number": quiz_number,
+	}
+	return render(request, "question.html", context)
 
 def completed(request, quiz_number):
-	return render(request, "results.html")
+	context = {
+		"correct": 12,
+		"total": 20,
+		"quiz_number": quiz_number, 
+	}
+	return render(request, "results.html", context)
