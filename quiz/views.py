f7eaac2953bb9b0f7f262b42e6396802cae96d65
Vertsi <v.vikstedt@gmail.com>
2017-09-28 15:25:16 +0200

    DB final

--- quiz/views.py
+++ quiz/views.py
@@ -1,52 +1,64 @@
-from django.shortcuts import render
 
-quizzes = [
-	{
-		"quiz_number": 1, 
-		"name": "Epic places",	
-		"description": "The 12 most epic places to visit in Barcelona"
-	},
-	{
-		"quiz_number": 2, 
-		"name": "Famous persons",	
-		"description": "The 10 most famous persons in Barcelona"
-	},
-	{
-		"quiz_number": 3, 
-		"name": "Hidden gems",	
-		"description": "The 11 hidden gems you need to know in Barcelona"
-	},
-]
+from django.shortcuts import redirect
+from quiz.models import Quiz
+from django.shortcuts import render
 
 def startpage (request):
 	context = {
-		"quizzes": quizzes, 
+		"quizzes": Quiz.objects.all(), 
 	}
 	return render(request, "start.html", context)
 
 def quiz(request, quiz_number):
 	context = {
-		"quiz": quizzes[quiz_number - 1],
+		"quiz": Quiz.objects.get(quiz_number=quiz_number),
 		"quiz_number": quiz_number, 
 	}
 	return render(request, "quiz.html", context)
 
 def question(request, quiz_number, question_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	question = questions[question_number - 1]
 	context = {
-		"question_number": question_number,
-		"question": "Barcelona is the third largest tourist destination in Europ after Paris and London - but how many tourists visit the city every year?",
-		"answer1": "20 million",
-		"answer2": "15 million",
-		"answer3": "8 million",
-		"quiz_number": quiz_number,
+    		"question_number": question_number,
+    		"question": question.question,
+	    	"answer1": question.answer1,
+    		"answer2": question.answer2,
+	    	"answer3": question.answer3,
+	    	"answer4": question.answer4,
+	    	"quiz": quiz,
+			"quiz_number": quiz_number,
 	}
 	return render(request, "question.html", context)
 
 def completed(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = list(quiz.questions.all())
+	saved_answers = request.session.get(str(quiz_number), {})
+	num_correct_answers = 0
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number) - 1].correct
+	if correct_answer == answer:
+		num_correct_answers = num_correct_answers + 1
+	num_questions = quiz.questions.count()
 	context = {
-		"correct": 12,
-		"total": 20,
+		"correct": num_correct_answers,
+		"total": num_questions,
 		"quiz_number": quiz_number,
-		"quizzes": quizzes, 
+		"quizzes": Quiz.objects.all(), 
 	}
 	return render(request, "results.html", context)
+
+def answer(request, quiz_number, question_number):
+	answer = request.POST["answer"]
+	saved_answers = request.session.get(str(quiz_number), {})
+	saved_answers[question_number] = int(answer)
+	request.session[quiz_number] = saved_answers
+
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	num_questions = quiz.questions.count()
+	if num_questions <= question_number:
+		return redirect("completed_page", quiz_number)
+	else:
+		return redirect("question_page", quiz_number, question_number + 1)
